b98b6e4eebebea78492149b87f49e839ca82bcad
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-04-03 16:33:36 +0300

    Scrollable details buttons

--- src/components/sections/filter/ui-components/details-buttons.tsx
+++ src/components/sections/filter/ui-components/details-buttons.tsx
@@ -5,39 +5,26 @@ interface DetailsButtonsProps {
   setActive: (value: string) => void;
 }
 
+const Data = [
+  "Kisisel Bilgiler",
+  "Is Deneyimleri",
+  "Yabanci Dil",
+  "Egitim",
+  "Ozel Durumlar",
+];
+
 const DetailsButtons = ({ active, setActive }: DetailsButtonsProps) => {
   return (
-    <div className="w-full flex gap-3 mb-5 min-w-[500px]">
-      <Button
-        variant={active === "Kisisel Bilgiler" ? "detailsActive" : "details"}
-        onClick={() => setActive("Kisisel Bilgiler")}
-      >
-        Kisisel Bilgiler
-      </Button>
-      <Button
-        variant={active === "Is Deneyimleri" ? "detailsActive" : "details"}
-        onClick={() => setActive("Is Deneyimleri")}
-      >
-        Is Deneyimleri
-      </Button>
-      <Button
-        variant={active === "Yabanci Dil" ? "detailsActive" : "details"}
-        onClick={() => setActive("Yabanci Dil")}
-      >
-        Yabanci Dil
-      </Button>
-      <Button
-        variant={active === "Egitim" ? "detailsActive" : "details"}
-        onClick={() => setActive("Egitim")}
-      >
-        Egitim
-      </Button>
-      <Button
-        variant={active === "Ozel Durumlar" ? "detailsActive" : "details"}
-        onClick={() => setActive("Ozel Durumlar")}
-      >
-        Ozel Durumlar
-      </Button>
+    <div className="w-full overflow-x-auto flex gap-3 mb-5 ">
+      {Data.map((item) => (
+        <Button
+          variant={active === item ? "detailsActive" : "details"}
+          onClick={() => setActive(item)}
+          key={item}
+        >
+          {item}
+        </Button>
+      ))}
     </div>
   );
 };
