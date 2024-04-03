f89bed5ab3b0d0308c4f60df0c70af60c98ee342
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-04-03 08:52:59 +0300

    Import component library

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./ui/button";
+
 const Main = () => {
-  return <div className="text-blue-500">hello</div>;
+  return (
+    <div className="text-blue-500">
+      hello
+      <Button variant="link">hehe</Button>
+    </div>
+  );
 };
 
 export default Main;
